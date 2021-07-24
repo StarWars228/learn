94fe61cb82bc6033dbb417c452a2d494c49ef08a
Sergey Viktorovich <sjkhh@mail.ru>
2021-07-24 20:47:25 +0300

    tested 1-4

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -1,7 +1,32 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task4 {
     public static void main(String[] args) {
 
+        int [] array = new int [10];
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10000);
+        }
+        System.out.println(Arrays.toString(array));
+
+        int maxSum = 0;
+        int maxSumIdx = 0;
+        for (int i = 0; i < array.length - 2; i++){
+            int sum = 0;
+            for(int k = i; k < i + 3; k++){
+                sum = array[k] + sum;
+            }
+            if (sum > maxSum){
+                maxSum = sum;
+                maxSumIdx = i;
+            }
+        }
+        System.out.println(maxSum + " Максимальная сумма 3х элементов");
+        System.out.println(maxSumIdx + " Индекс максимальной суммы");
     }
 }
